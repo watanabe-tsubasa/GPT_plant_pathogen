78e55545f4cb7435449b74351a605777a1b63bb2
Watanabe Tsubasa (渡邊 翼) <watanabe-tsuba@aeonpeople.biz>
2023-04-19 19:03:36 +0900

    upd

--- test.js
+++ test.js
@@ -1,16 +1,42 @@
 import { Configuration, OpenAIApi } from "openai";
 const configuration = new Configuration({
     organization: "org-e3bH006RqIJewGofzKDFTQaV",
-    apiKey: process.env.OPENAI_API_KEY || 'sk-',
+    apiKey: process.env.OPENAI_API_KEY || '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',
 });
 const openai = new OpenAIApi(configuration);
 const completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
-    messages: [{
-        role: "user", 
-        content: "初めてのNode.jsからの会話です。"
-    }],
-    n:1,
+    messages: [
+        {
+            role: "user", 
+            content: '犬の種類について会話をした後に、適当な犬種についてのJSONデータを生成してください。JSONのフォーマットは' +
+            '    {\n' +
+            '      "name": "",\n' +
+            '      "origin": "",\n' +
+            '      "temperament": "",\n' +
+            '      "size": "",\n' +
+            '    }' +
+            'こちらでお願いします。'
+        },
+    ],
+    temperature: 0.5,
+    n: 1,
   });
 
-console.log(completion.data.choices[0].message);
+console.log(completion.data);
+const content = completion.data.choices[0].message.content;
+console.log(content);
+
+const regex = /{[\s\S]*}/;
+const match = content.match(regex);
+
+let message;
+if (match) {
+    message = JSON.parse(match[0]);
+} else {
+    message = content;
+}
+
+console.log('会話全体を出力\n');
+console.log(message);
+console.log(`name_keyを出力:${message.name}\ntemperament_keyを出力:${message.temperament}`);
